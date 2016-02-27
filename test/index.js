f9908f29d6436d9b2aa65348b31703448f847ce1
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-02-27 11:29:20 +0100

    test updates

--- test/index.js
+++ test/index.js
@@ -4,10 +4,10 @@ const fs = require('fs')
 
 let posthtml = require('posthtml')
 
-let lint = require('../index')()
+let hint = require('../index')()
 
 let html = fs.readFileSync('./index.html', 'utf-8')
 
-posthtml([ lint ])
+posthtml([ hint ])
   .process(html)
   .then(result => result.html)
